7f4bc1799b7b13f9ba224a7327577affee7419cf
delude88 <tobias.hegemann@googlemail.com>
2020-09-30 15:42:33 +0200

    fixed ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,13 @@ module.exports = {
         instances: 1,
         autorestart: true,
         watch: false,
-        env_production: {
-            "NODE_ENV": "production",
-            "ENV_PATH": "/home/node/.env",
-        },
         max_memory_restart: '1G',
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            "NODE_ENV": "production"
+        },
     }],
 
     deploy: {
@@ -27,8 +29,7 @@ module.exports = {
                 "AUTH_URL": "https://auth.digital-stage.org",
                 "MONGO_URL": "mongodb://10.114.0.4:27017",
                 "PORT": 3000,
-                "NODE_ENV": "production",
-                "ENV_PATH": "/home/node/.env",
+                "NODE_ENV": "production"
             },
             'post-deploy': 'npm install && npm run build && pm2 restart ecosystem.config.js --env production'
         }
